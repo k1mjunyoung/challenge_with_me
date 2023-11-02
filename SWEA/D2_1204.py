1ce72fdcaa3878f35a4e037eb85e0b2842b7d7e6
nayoung03 <naje311@naver.com>
2023-11-02 18:25:51 +0900

    #2

--- SWEA/D2_1204.py
+++ SWEA/D2_1204.py
@@ -1,8 +1,27 @@
 #최빈수 구하기 
 
 
+# t = int(input()) 
+# # input data 1000개, 시간 : 10s 
+# for i in range(t): 
+#     n = int(input()) # 테스트 케이스 번호 
+#     arr =[0]*1000 
+#     for _ in range(1000):
+#         tmp = int(input())
+#         arr[tmp] +=1 
+#     sum = max(arr) 
+#     print("#{} {}".format(i,sum))
+
+
+
 t = int(input()) 
 # input data 1000개, 시간 : 10s 
 for i in range(t): 
     n = int(input()) # 테스트 케이스 번호 
-    
+    arr =[0]*10 
+    for i in range(10):
+        tmp = int(input()) 
+        arr[tmp] +=1 
+    sum = max(arr) 
+    print("#{} {}".format(i,sum))
+
