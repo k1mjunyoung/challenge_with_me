0c27a9825a69a39ed08c5610752ecbdb756450db
nayoung03 <naje311@naver.com>
2023-11-07 16:14:22 +0900

    231107 2version

--- SWEA/D2_1954.py
+++ SWEA/D2_1954.py
@@ -1,44 +1,47 @@
 #달팽이 숫자 
 # algorithm type : 2차원 배열 리스트  - 다중 리스트 좌표개념  
-
 '''
-상하 좌우 
-dr = [-1,1,0,0]
-dc = [0,0,-1,1]
+초기 :  i=0.j=0,cnt=1 arr[i][j] = cnt.cnt+=1 
+while cnt<=N*N : # 범위 이내인 경우 
+다음 위치 계산 : ni, nj 
+di = [0,1,0,-1] dj =[1,0,-1,0]
+dr = 0
+ni = i+di[dr] 
+dj = j+dj[dr]
+if 0<=ni<N and 0<=nj<N  and anrr[ni]]nj] == 0 인 경우 해당 
+좌표이동 : i,j = ni,nj  arr[i][j]=cnt cnt+=1 
+else dr = (dr +1) % 4 
+
+
+import sys 
+sys.stdin= open("input.txt","r") 
+
+
 '''
 
 #input() 
-t = int(input()) 
+T = int(input()) 
 # 달팽이의 이동방향 ; 우>하>좌>상 , 방향 전환 리스트를 만들어준다. 
 di = [0,1,0,-1]
 dj = [1,0,-1,0]
 
-for tc in range(1,t+1): 
+for  test_case in range(1,T+1): 
     N = int(input())
-    arr = [[0]*N for _ in range(N)] # 2차원 배열 초기화 
-
+    arr = [[0]*N for _ in range(N)] # 2차원 배열 초기화 list conprehension 
     i,j,cnt,dr=0,0,1,0  #초기화
     arr[i][j] = cnt
     cnt +=1 
 
-    while cnt <= N*N:
+    while cnt <= N*N: # 적을 값이 범위 이내라면 
         ni,nj = i+di[dr],j+dj[dr] 
-        if 0<=ni<N and 0<=nj<N and arr[ni][nj] ==0 : 
-            i,j= ni,nj
+        if 0<=ni<N and 0<=nj<N and arr[ni][nj] == 0: 
+            i,j= ni,nj  #좌표 이동 
             arr[i][j] = cnt 
             cnt +=1 
-
         else: 
-            dr = (dr+1)%4
+            dr = (dr+1)%4 # 방향을 순환해서 돌아야하니까 
         
     
-    print("#"+str(tc))
+    print(f'#{test_case}')
     for list in arr:
         print(*list)
-
-
-
-
-# 방향 전환이 일어나기 전까지 값을 더한다. 
-# row 나 column 인덱스가 범위 밖인 경우 or 이미 방문한(숫자가 입력된 상태) 일 경우 
-
