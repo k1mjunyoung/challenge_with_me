0fdcbadb28e111510ef214f08f2b8f65822740a3
nayoung03 <naje311@naver.com>
2023-11-07 18:28:12 +0900

    2차

--- SWEA/D2_2001.py
+++ SWEA/D2_2001.py
@@ -1,8 +1,14 @@
 # 파리퇴치 
 # input 
 T = int(input())
+arr=[] 
 for i in range(1,T+1): 
     N,M = map(input().split()) 
+    arr =# 2차원 리스트 초기화 
+
 
     dx = [1,-1,0,0]  #상하
     dy = [0,0,-1,1]  #좌우 
+
+
+
