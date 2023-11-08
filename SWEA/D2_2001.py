9f0ee62757226525ae67022f523a0f4263493ecd
nayoung03 <naje311@naver.com>
2023-11-08 17:04:05 +0900

    231108 파리퇴치

--- SWEA/D2_2001.py
+++ SWEA/D2_2001.py
@@ -1,14 +1,23 @@
 # 파리퇴치 
 # input 
+
 T = int(input())
-arr=[] 
-for i in range(1,T+1): 
-    N,M = map(input().split()) 
-    arr =# 2차원 리스트 초기화 
+for testcase in range(1,T+1): 
+    N,M = map(int,input().split()) 
+    arr =[list(map(int,input().split())) for _ in range(N)]# 2차원 리스트 초기화 
+    kills = []
+    #파리채를 내려칠 곳 탐색 
+    for i in range(N-M+1): 
+        for j in range(N-M+1): 
+            fly=0 
+            #해당 위치를 타격했을 때 잡을 수 있는 파리의 수 탐색 
+            for k in range(M): 
+                 for l in range(M): 
+                      fly+=arr[i+k][j+l]
+            kills.append(fly)         
 
 
-    dx = [1,-1,0,0]  #상하
-    dy = [0,0,-1,1]  #좌우 
+    print("#{} {}".format(testcase,max(kills)))
 
 
 
