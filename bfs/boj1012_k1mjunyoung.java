31d4613e022fd685b468dd5d3a9128df0226feee
k1mjunyoung <vovrwm@gmail.com>
2023-11-22 12:19:45 +0900

    [S2]BOJ1012: 유기농 배추

--- bfs/boj1012_k1mjunyoung.java
+++ bfs/boj1012_k1mjunyoung.java
@@ -1,4 +1,4 @@
-package array;
+package bfs;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -11,13 +11,26 @@ import java.util.StringTokenizer;
  * https://www.acmicpc.net/problem/1012
  */
 public class boj1012_k1mjunyoung {
-    // 상 우 하 좌
+    // 1. 상태 정의
+    static class State {
+        int x;
+        int y;
+        
+        State(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    // 상 우 하 좌(시계 방향)
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {-1, 0, 1, 0};
 
-    static int N, M;
+    static int M, N;
+
     static int[][] field;
-    static int[][] check;
+    static boolean[][] isVisited;
+    static boolean[][] isGroupVisited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -34,13 +47,17 @@ public class boj1012_k1mjunyoung {
             // 세로길이
             N = Integer.parseInt(st.nextToken());
 
+            // 밭 생성
             field = new int[N][M];
-            check = new int[N][M];
+            
+            // 방문 검사 배열
+            isVisited = new boolean[N][M];
+            isGroupVisited = new boolean[N][M];
 
             // 배추가 심어진 위치의 개수
             int K = Integer.parseInt(st.nextToken());
 
-            // 밭 생성
+            // 배추 심기
             for (int i = 0; i < K; i++) {
                 st = new StringTokenizer(br.readLine(), " ");
                 int X = Integer.parseInt(st.nextToken());
@@ -50,13 +67,34 @@ public class boj1012_k1mjunyoung {
 
             // 필요한 지렁이 수
             int worm = 0;
+            
+            Queue<State> queue = new LinkedList<>();
+            queue.add(new State(0, 0));
+            
+            while (!queue.isEmpty()) {
+                State state = queue.poll();
 
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < M; j++) {
-                    if (field[i][j] == 1 && check[i][j] != 1) {
-                        bfs(i ,j);
-                        worm++;
+                for (int d = 0; d < 4; d++) {
+                    int nx = state.x + dx[d];
+                    int ny = state.y + dy[d];
+
+                    if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
+                        continue;
                     }
+
+                    if (field[ny][nx] == 1) {
+                        findGroup(nx, ny);
+                        field[ny][nx] = 0;
+                        worm++;
+                        continue;
+                    }
+
+                    if (isVisited[ny][nx]) {
+                        continue;
+                    }
+
+                    queue.add(new State(nx, ny));
+                    isVisited[ny][nx] = true;
                 }
             }
 
@@ -64,37 +102,34 @@ public class boj1012_k1mjunyoung {
         }
     }
 
-    static void bfs(int x, int y) {
-        Queue<Pos> queue = new LinkedList<>();
-        queue.offer(new Pos(x, y));
-        check[x][y] = 1;
+    static void findGroup(int x, int y) {
+        Queue<State> groupQueue = new LinkedList<>();
+        groupQueue.add(new State(x, y));
 
-        // 배추가 있는 좌표 check[][]에 저장
-        while (!queue.isEmpty()) {
-            Pos pos = queue.poll();
+        while (!groupQueue.isEmpty()) {
+            State groupState = groupQueue.poll();
 
-            for (int k = 0; k < 4; k++) {
-                int nx = pos.x + dx[k];
-                int ny = pos.y + dy[k];
+            for (int d = 0; d < 4; d++) {
+                int nx = groupState.x + dx[d];
+                int ny = groupState.y + dy[d];
 
                 if (nx < 0 || nx >= M || ny < 0 || ny >= N) {
                     continue;
                 }
 
-                if (field[ny][nx] == 1 && check[ny][nx] != 1) {
-                    queue.offer(new Pos(nx, ny));
-                    check[ny][nx] = 1;
+                if (field[ny][nx] != 1) {
+                    continue;
                 }
-            }
-        }
-    }
-}
-class Pos {
-    int x;
-    int y;
 
-    Pos(int x, int y) {
-        this.x = x;
-        this.y = y;
+                if (isGroupVisited[ny][nx]) {
+                    continue;
+                }
+
+                groupQueue.add(new State(nx, ny));
+                isGroupVisited[ny][nx] = true;
+            }
+
+            field[groupState.y][groupState.x] = 0;
+        }
     }
 }
